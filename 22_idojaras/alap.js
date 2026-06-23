76fa7d177223bf05685c50b501ed9579a15557de
Aniko <nahajia@gmail.com>
2026-06-23 19:24:57 +0200

    adatb 3 darab

--- 22_idojaras/alap.js
+++ 22_idojaras/alap.js
@@ -3,6 +3,35 @@ async function letoltes() {
     let szelesseg=document.getElementById("szelesseg").value 
     let hosszusag=document.getElementById("hosszusag").value 
     
+    //terkep
+    var data = [{
+  type:'scattermap',
+  lat:[`${szelesseg}`],
+  lon:[`${hosszusag}`],
+  mode:'markers',
+  marker: {
+    size:14
+  },
+  text:['']
+}]
+
+var layout = {
+  autosize: true,
+  hovermode:'closest',
+  map: {
+    bearing:0,
+    center: {
+      lat:szelesseg,
+      lon:hosszusag
+    },
+    pitch:0,
+    zoom:5
+  },
+}
+
+Plotly.newPlot('terkep', data, layout)
+
+    //idojaras letöltése
     let x = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${szelesseg}&longitude=${hosszusag}&hourly=temperature_2m`);
     let y = await x.json();
     megjelenit(y);
